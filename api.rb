417a5d618ef670783ea230a865ca73704621bef8
Sébastien Saunier <seb@saunier.me>
2020-07-13 12:00:57 +0200

    Add example

--- api.rb
+++ api.rb
@@ -14,18 +14,22 @@ $redis = Redis.new
 
 get '/' do
   url = params[:url]
-  data = $redis.get(url)
-  if data.nil?
-    response = HTTP.follow.get(url)
-    open_graph = OGP::OpenGraph.new(response.to_s)
-    url = response.uri.to_s
-    data = open_graph.data
-    $redis.set(url, JSON.generate(data))
-    $redis.expire(url, 60 * 60 * 24)
+  if url.nil? || url == ""
+    json(error: "Please provide a URL to scrape", example_url: "https://opengraph.lewagon.com/?url=https://www.lewagon.com")
   else
-    data = JSON.parse(data)
+    data = $redis.get(url)
+    if data.nil?
+      response = HTTP.follow.get(url)
+      open_graph = OGP::OpenGraph.new(response.to_s)
+      url = response.uri.to_s
+      data = open_graph.data
+      $redis.set(url, JSON.generate(data))
+      $redis.expire(url, 60 * 60 * 24)
+    else
+      data = JSON.parse(data)
+    end
+    json(data: data, url: url)
   end
-  json(data: data, url: url)
 rescue OGP::MissingAttributeError
   json(error: "No og: attributes found for this website")
 rescue StandardError => e
